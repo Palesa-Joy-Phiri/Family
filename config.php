c2a428f60f4d6eff6b45f6728c1dc77bfa142ec0
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-10-17 22:39:45 +0200

    Changed password

--- config.php
+++ config.php
@@ -2,9 +2,14 @@
 
 define ('DB_NAME', 'Family');
 define ('DB_USER', 'Admin');
-define ('DB_PASSWORD','P@1e961211');
+define ('DB_PASSWORD','@Dmin123');
 define ('DB_HOST', 'localhost');
 
-$connection=
+$connection= @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+
+if(!$connection) {
+    die( 'Could not connect'.mysqli_connect_error());
+}
+
 
 ?>
