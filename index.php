16ae2a7b9c40ea8f0ed807c91247f92a66d09708
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-10-17 22:40:15 +0200

    Php Mysql connection made

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> Family Member Database</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+
+    <body>
+
+        <div id="container">
+            <h2> My family</h2>
+            <div id="btn_add">
+                <a href="#"> Add </a>
+            </div>
+        </div>
+
+        <div id="content">
+ 
+            <!-- Start PHP to connect config file-->
+            <?php
+                require_once(config.php); #calling the config file and passing the path as a parameter now we can access the database in that file
+
+                $sql = "SELECT MemberId, Firstname, Lastname, Age, Gender, Relationship FROM FamilyMembers";
+                $response = @mysqli_query($connection, $sql);
+
+                if($response) {
+
+                    #tr stands for table row and th means table heading
+                    echo '<table>
+                    
+                        <tr> 
+                            <th> Member Id </th>
+                            <th> First Name</th>
+                            <th> Last Name</th>
+                            <th> Age </th>
+                            <th> Gender </th>
+                            <th> Relationship </th>
+                            
+                        </tr>';
+
+                        while ($row = mysqli_fetch_array(response)) {
+                            echo '
+
+                                <tr>
+
+                                <td> '.$row ['MemberID'].' </td>
+                                <td> '.$row ['FirstName'].' </td>
+                                <td> '.$row ['LastName'].' </td>
+                                <td> '.$row ['Age'].' </td>
+                                <td> '.$row ['Gender'].' </td>
+                                <td> '.$row ['Relationship'].' </td>
+
+                                </tr>';
+
+                        }
+                    
+                        echo '</table>';
+                }
+
+                else {
+                    echo "Could not get a response from database".mysqli_error(connection);
+                }
+
+                mysqli_close($connection);
+
+
+            ?>
+
+        </div>
+
+    </body>
+</html>
