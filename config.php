5e0b29a98ff1f36ed3f9971d23e800141294d055
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-10-20 06:56:48 +0200

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define ('DB_HOST', 'localhost');
 $connection= @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 
 if(!$connection) {
-    die( 'Could not connect'.mysqli_connect_error());
+    die( 'Could not connect:'.mysqli_connect_error());
 }
 
 
